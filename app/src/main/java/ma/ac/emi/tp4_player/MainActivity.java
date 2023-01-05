86736e1c3594c9629819cf0728c434d1d6ccfede
Spec2uloos <monimone18@gmail.com>
2023-01-05 19:32:11 +0100

    Initial commit

---
+++ app/src/main/java/ma/ac/emi/tp4_player/MainActivity.java
@@ -0,0 +1,14 @@
+package ma.ac.emi.tp4_player;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
